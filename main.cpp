fbd5f7b0dd17a01b749a523fdec1f1687dc68782
Mrmayman <119435518+Mrmayman@users.noreply.github.com>
2023-05-16 17:55:42 +0530

    removed unused code, documented controller

--- main.cpp
+++ main.cpp
@@ -9,14 +9,6 @@
 #include <stdio.h>
 #include <time.h>
 
-//#ifdef _WIN32
-//#include <direct.h>
-//#define GetCurrentDir _getcwd
-//#else
-//#include <unistd.h>
-//#define GetCurrentDir getcwd
-//#endif
-
 // internal C++ libraries
 #include <iostream>
 #include <filesystem>
@@ -100,6 +92,45 @@ int main(int argc, char *argv[])
         delta = current_time - last_time;
         last_time = current_time;
 
+        /*
+        bool isAPressed = SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_A);
+if (isAPressed) {
+    // Handle 'A' button press
+}
+        You can replace SDL_CONTROLLER_BUTTON_A with any of the following constants to check for other buttons on the game controller:
+
+    SDL_CONTROLLER_BUTTON_B
+    SDL_CONTROLLER_BUTTON_X
+    SDL_CONTROLLER_BUTTON_Y
+    SDL_CONTROLLER_BUTTON_BACK
+    SDL_CONTROLLER_BUTTON_GUIDE
+    SDL_CONTROLLER_BUTTON_START
+    SDL_CONTROLLER_BUTTON_LEFTSTICK
+    SDL_CONTROLLER_BUTTON_RIGHTSTICK
+    SDL_CONTROLLER_BUTTON_LEFTSHOULDER
+    SDL_CONTROLLER_BUTTON_RIGHTSHOULDER
+    SDL_CONTROLLER_BUTTON_DPAD_UP
+    SDL_CONTROLLER_BUTTON_DPAD_DOWN
+    SDL_CONTROLLER_BUTTON_DPAD_LEFT
+    SDL_CONTROLLER_BUTTON_DPAD_RIGHT
+    
+        SDL_CONTROLLER_BUTTON_LEFTSHOULDER: for the left shoulder button
+    SDL_CONTROLLER_BUTTON_RIGHTSHOULDER: for the right shoulder button
+    SDL_CONTROLLER_BUTTON_LEFT_TRIGGER: for the left trigger
+    SDL_CONTROLLER_BUTTON_RIGHT_TRIGGER: for the right trigger
+    
+    shoulder is the hotbar, trigger is the action
+    
+        Triangle button -> SDL_CONTROLLER_BUTTON_Y
+    Circle button -> SDL_CONTROLLER_BUTTON_B
+    Cross button -> SDL_CONTROLLER_BUTTON_A
+    Square button -> SDL_CONTROLLER_BUTTON_X
+    
+        SELECT button -> SDL_CONTROLLER_BUTTON_BACK
+    START button -> SDL_CONTROLLER_BUTTON_START
+    PS button -> SDL_CONTROLLER_BUTTON_GUIDE
+        */
+        
         if (keyboard_state[SDL_SCANCODE_D]) { entities[0].speedX += xspeed * delta; }
         if (keyboard_state[SDL_SCANCODE_A]) { entities[0].speedX -= xspeed * delta; }
         joyxa = SDL_GameControllerGetAxis(gameController, SDL_CONTROLLER_AXIS_LEFTX);
